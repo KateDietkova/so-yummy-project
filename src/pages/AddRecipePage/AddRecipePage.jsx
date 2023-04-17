05120aca32451b01ff6b637f996115e49191ad24
yulia-mazurenko <yulia.mazurenko1@gmail.com>
2023-04-17 14:05:42 +0300

    commented adding of mait title

--- src/pages/AddRecipePage/AddRecipePage.jsx
+++ src/pages/AddRecipePage/AddRecipePage.jsx
@@ -1,11 +1,11 @@
 import { AddRecipeForm } from 'components/AddRecipePageComponents/AddRecipeForm/AddRecipeForm';
-import { MainTitle } from '../../components/universalComponents/MainTitle/MainTitle';
+// import { MainTitle } from '../../components/universalComponents/MainTitle/MainTitle';
 import { AddRecipeContainer } from './AddRecipePage.styled';
 
 const AddRecipePage = () => {
   return (
     <AddRecipeContainer>
-      <MainTitle text="Add recipe" />
+      {/* <MainTitle text="Add recipe" /> */}
       <AddRecipeForm />
     </AddRecipeContainer>
   );
