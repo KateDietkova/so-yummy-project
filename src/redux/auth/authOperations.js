9fe7368b8fecbafe290c502b2584d93ca5421872
KorotkayaNastuha <skiteikina11@gmail.com>
2023-04-18 21:17:18 +0300

    registerlogin

--- src/redux/auth/authOperations.js
+++ src/redux/auth/authOperations.js
@@ -3,13 +3,45 @@ import axios from 'axios';
 
 axios.defaults.baseURL = ' https://so-yummy-api.herokuapp.com';
 
-const token = {
-  set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-  },
-
-  unset() {
-    axios.defaults.headers.common.Authorization = '';
-  },
+const setToken = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
+const unSetToken = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkApi) => {
+    try {
+      const response = await axios.post('/auth/registration', credentials);
+      setToken(response.data.token);
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message)
+    }
+  }
+)
+export const login = createAsyncThunk(
+    'auth/login',
+    async (credentials, thunkAPI) => {
+        try {
+          const response = await axios.post('/auth/login', credentials);
+            setToken(response.data.token);
+            return response.data;
+        } catch (error) {
+          return thunkAPI.rejectWithValue(error.message);
+      }
+    }
+);
+
+export const logout = createAsyncThunk("/auth/logout",
+  async (_, thunkAPI) => {
+    try {
+        await axios.get("/auth/logout");
+        unSetToken();
+    } catch (error) {
+        return thunkAPI.rejectWithValue(error.message);
+    }
+});
