61243a6f7ccbaa51e29d664a7a7bba53f6d36ac3
Katerina Dietkova <katrinkir1296@gmail.com>
2023-04-18 17:51:00 +0300

    add base redux files for recipes and ingredients

---
+++ src/redux/ingredients/ingredientsSelectors.js
@@ -0,0 +1,5 @@
+export const selectIngredients = state => state.ingredients.ingredients;
+export const selectShoppingList = state => state.ingredients.shoppingList;
+
+export const selectIsLoading = state => state.ingredients.isLoggedIn;
+export const selectRecipesError = state => state.ingredients.error;
