d472b863b70ee5b2c944bc90558bc15ca35bde10
MartaSyvyk <shavaliuk.marta@gmail.com>
2023-04-24 22:06:43 +0300

    Update AddRecipeForm.jsx

--- src/components/AddRecipePageComponents/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipePageComponents/AddRecipeForm/AddRecipeForm.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Formik } from 'formik';
 import { descrFieldsSchema } from './validationAddRecipe';
