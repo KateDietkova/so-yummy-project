6adad289c5510a2678fd838fe8eaee22e5d1b73c
YuliiaMakhovska <y.makhovska@gmail.com>
2023-04-27 10:30:25 +0300

    fix styles for leaves

--- src/pages/CategoriesPage/CategoriesPage.styled.js
+++ src/pages/CategoriesPage/CategoriesPage.styled.js
@@ -7,3 +7,11 @@ export const CategoriesPageContainer = styled(Container)`
       ? props.theme.colors.bgdarkTheme
       : props.theme.colors.textWhite};
 `;
+
+export const CategoriesWrapper = styled.div`
+  margin-bottom: 70px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 150px;
+  }
+`;
