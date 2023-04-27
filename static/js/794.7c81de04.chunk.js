04a5498fa7ed5e5a3f5ccf3051c188f49fbb26a1
KateDietkova <38350154+KateDietkova@users.noreply.github.com>
2023-04-27 14:28:09 +0000

    Deploying to gh-pages from @ KateDietkova/so-yummy-project@378cf3e3cf11cfed51790681ad0333be4bcadc2f 🚀

--- static/js/794.7c81de04.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[794],{1794:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("h1",{children:"NotFoundPage"})}}}]);
-//# sourceMappingURL=794.7c81de04.chunk.js.map
