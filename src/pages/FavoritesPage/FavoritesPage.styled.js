041935f355cd98bc52b0223313ee9a907dda0e73
LizaPoperechna <poperechnayaliza@gmail.com>
2023-04-28 01:31:37 -0400

    favorite
    added option "empty list"

--- src/pages/FavoritesPage/FavoritesPage.styled.js
+++ src/pages/FavoritesPage/FavoritesPage.styled.js
@@ -8,3 +8,23 @@ export const MyFavoriteContainer = styled(Container)`
       ? props.theme.colors.bgdarkTheme
       : props.theme.colors.textWhite};
 `;
+
+export const H3 = styled.h3`
+      font-size: ${props => props.theme.fontSizes.s};
+    line-height: 1;
+    font-style: normal;
+    font-weight: 500;
+    text-align: center;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    color: ${props => {
+          return props.theme.darkTheme
+              ? props.theme.colors.textWhite
+              : props.theme.colors.bgBlackDark
+  }};
+
+     @media screen and (min-width: 768px) {
+      font-size: ${props => props.theme.fontSizes.l};
+      line-height: 1;
+    }
+`
