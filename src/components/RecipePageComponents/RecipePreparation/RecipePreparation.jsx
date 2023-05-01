2d1a472283c1541bf0c087bc868a9527dfd9071f
Katerina Dietkova <katrinkir1296@gmail.com>
2023-05-01 14:12:44 +0300

    remove logs

--- src/components/RecipePageComponents/RecipePreparation/RecipePreparation.jsx
+++ src/components/RecipePageComponents/RecipePreparation/RecipePreparation.jsx
@@ -10,31 +10,30 @@ import {
 } from './RecipePreparation.styled';
 import IngredientsPlaceholder from '../../../assets/images/desktop/mocks/recipe-photo-default-2x.jpg';
 export const RecipePreparation = ({ instructions, preview }) => {
-
   let step = 0;
-  console.log(instructions);
   return (
     <PreparationSection>
       <div>
         <Title>Recipe Preparation</Title>
         <PrepeaRecipe>
           {instructions &&
-            instructions.replace(/(\n\s*\n|\n)/g, "").split('.').map((text, i) => {
-              console.log("instructions.split('.')", instructions.split('.'));
-              if (text === '' || text.length < 5) {
-                return null;
-              }
-              console.log('text', text);
-              step += 1;
-              return (
-                <ListItem key={i}>
-                  <Step>
-                    <Steps>{step}</Steps>
-                  </Step>
-                  <ListText>{text}</ListText>
-                </ListItem>
-              );
-            })}
+            instructions
+              .replace(/(\n\s*\n|\n)/g, '')
+              .split('.')
+              .map((text, i) => {
+                if (text === '' || text.length < 5) {
+                  return null;
+                }
+                step += 1;
+                return (
+                  <ListItem key={i}>
+                    <Step>
+                      <Steps>{step}</Steps>
+                    </Step>
+                    <ListText>{text}</ListText>
+                  </ListItem>
+                );
+              })}
         </PrepeaRecipe>
       </div>
       <PrepeaRecipeImage src={preview ? preview : IngredientsPlaceholder} />
