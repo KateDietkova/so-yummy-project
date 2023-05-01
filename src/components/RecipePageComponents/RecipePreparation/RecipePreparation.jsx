899a02e65083400c972671042ca3f69ceef7dacb
Katerina Dietkova <katrinkir1296@gmail.com>
2023-05-01 13:40:26 +0300

    fix steps function

--- src/components/RecipePageComponents/RecipePreparation/RecipePreparation.jsx
+++ src/components/RecipePageComponents/RecipePreparation/RecipePreparation.jsx
@@ -1,46 +1,38 @@
 import {
-    PreparationSection,
-    Title,
-    PrepeaRecipe,
-    PrepeaRecipeImage,
-    Steps,
-    Step,
-    ListItem,
-    ListText
+  PreparationSection,
+  Title,
+  PrepeaRecipe,
+  PrepeaRecipeImage,
+  Steps,
+  Step,
+  ListItem,
+  ListText,
 } from './RecipePreparation.styled';
-import IngredientsPlaceholder from '../../../assets/images/desktop/mocks/recipe-photo-default-2x.jpg'
+import IngredientsPlaceholder from '../../../assets/images/desktop/mocks/recipe-photo-default-2x.jpg';
 export const RecipePreparation = ({ instructions, preview }) => {
-  
-  
   console.log(instructions);
   return (
     <PreparationSection>
-      <div >
-      <Title>Recipe Preparation</Title>
+      <div>
+        <Title>Recipe Preparation</Title>
         <PrepeaRecipe>
-          {instructions && 
-            instructions.replace('.').split('. ').map((text, i) => {
-              const isDropEnd = text[text.length - 1] === ".";
-              console.log(isDropEnd);
-              const textUpdate = isDropEnd ? text : text.slice(0,text.length - 1)
-              console.log('text', text);
-              console.log('text.slice', text.slice(0,text.length - 1));
+          {instructions &&
+            instructions.split('.').map((text, i) => {
+              if (text === '') {
+                return null;
+              }
               return (
-                <ListItem key={i} >
+                <ListItem key={i}>
                   <Step>
-                  <Steps>{i + 1}</Steps>
+                    <Steps>{i + 1}</Steps>
                   </Step>
-                 <ListText>{textUpdate}</ListText> 
+                  <ListText>{text}</ListText>
                 </ListItem>
-              )
+              );
             })}
-          </PrepeaRecipe>
-      </div>  
-      <PrepeaRecipeImage
-        src={preview ? preview : IngredientsPlaceholder}
-      />
-       
+        </PrepeaRecipe>
+      </div>
+      <PrepeaRecipeImage src={preview ? preview : IngredientsPlaceholder} />
     </PreparationSection>
   );
 };
-
