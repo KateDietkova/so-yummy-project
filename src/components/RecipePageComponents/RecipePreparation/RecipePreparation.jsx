5c8b5bd5fd5d47f12d0f553897e4d62995a8724e
KorotkayaNastuha <skiteikina11@gmail.com>
2023-05-01 13:08:08 +0300

    step

--- src/components/RecipePageComponents/RecipePreparation/RecipePreparation.jsx
+++ src/components/RecipePageComponents/RecipePreparation/RecipePreparation.jsx
@@ -9,8 +9,10 @@ import {
     ListText
 } from './RecipePreparation.styled';
 import IngredientsPlaceholder from '../../../assets/images/desktop/mocks/recipe-photo-default-2x.jpg'
-export const RecipePreparation = ({instructions, preview}) => {
-
+export const RecipePreparation = ({ instructions, preview }) => {
+  
+  
+  console.log(instructions);
   return (
     <PreparationSection>
       <div >
@@ -18,12 +20,17 @@ export const RecipePreparation = ({instructions, preview}) => {
         <PrepeaRecipe>
           {instructions && 
             instructions.replace('.').split('. ').map((text, i) => {
+              const isDropEnd = text[text.length - 1] === ".";
+              console.log(isDropEnd);
+              const textUpdate = isDropEnd ? text : text.slice(0,text.length - 1)
+              console.log('text', text);
+              console.log('text.slice', text.slice(0,text.length - 1));
               return (
                 <ListItem key={i} >
                   <Step>
                   <Steps>{i + 1}</Steps>
                   </Step>
-                 <ListText>{text}</ListText> 
+                 <ListText>{textUpdate}</ListText> 
                 </ListItem>
               )
             })}
